4c6024db2536aa2722606f077b90ac55ae118475
Abelaash <abelaash.giritharan@gmail.com>
2023-12-11 13:54:42 -0500

    add new project to repo

---
+++ src/main/java/com/example/finalDistributionCenter/demo/FinalDistributionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalDistributionCenter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDistributionCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalDistributionCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finalDistributionCenter/demo/FinalDistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalDistributionCenter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalDistributionCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
